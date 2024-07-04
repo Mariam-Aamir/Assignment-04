a78b8190921ee67818ffe9640a239770aa7908d1
[Mariam-Aamir] <[ummeareesh691@gmail.com]>
2024-07-04 14:03:31 +0500

    forth assignment

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+//Assingment 04
+//storing  the famous Quote and its Author
+let famousQuote = "Seek knowledge from the cradle to the grave";
+let famousAuthor = "Muhammad P.B.U.H ";
+//printing the message
+console.log(`"${famousQuote}" - ${famousAuthor}`);
---
+++ main.ts
@@ -0,0 +1,19 @@
+//Assingment 04
+
+//storing  the famous Quote and its Author
+
+
+let famousQuote:string = "Seek knowledge from the cradle to the grave"
+
+
+let famousAuthor:string = "Muhammad P.B.U.H "
+
+//printing the message
+
+console.log(`"${famousQuote}" - ${famousAuthor}`);
+
+
+
+
+
+
